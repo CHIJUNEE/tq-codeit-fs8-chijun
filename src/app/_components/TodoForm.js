99107b99e6598ccc86ff53eedac26819cbdac747
DESKTOP-14A5R6M\clwns <clwns00@naver.com>
2025-09-23 13:49:49 +0900

    usemutation error 구현완료

--- src/app/_components/TodoForm.js
+++ src/app/_components/TodoForm.js
@@ -1,13 +1,26 @@
 "use client";
 
+import { addTodo } from "@/api/todos";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useState } from "react";
 
-export default function TodoForm({ loadTodos }) {
+export default function TodoForm() {
   const [title, setTitle] = useState("");
 
+  const todoAdd = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+    onError: (err) => {
+      alert("할 일을 추가하는데 실패했습니다.", err);
+    },
+  });
+  const queryClient = useQueryClient();
   const handleAddTodo = async (title) => {
     try {
-      await addTodo(title);
+      todoAdd.mutate(title);
+      // await queryClient.invalidateQueries({ queryKey: ["todos"] });
       // TODO: 할 일 추가 후 목록 다시 불러오기
     } catch (err) {
       console.error("할 일 추가 중 오류 발생:", err);
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { use, useEffect, useState } from "react";
 import TodoList from "./_components/TodoList";
 import { fetchTodos } from "../api/todos.js";
 import { useQuery } from "@tanstack/react-query";
+import TodoForm from "./_components/TodoForm";
 
 export default function Home() {
   // const [todos, setTodos] = useState([]);
@@ -22,36 +23,15 @@ export default function Home() {
   //     setIsLoading(false);
   //   }
   // };
-  const {
-    data: todos,
-    isPending: isLoading,
-    error,
-  } = useQuery({
-    queryKey: ["todos"],
-    queryFn: fetchTodos,
-  });
 
-  // useEffect(() => {
-  //   loadTodos();
-  // }, []);
-  if (isLoading) {
-    return (
-      <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
-    );
-  } else if (error) {
-    return (
-      <div className="container mx-auto px-4 py-8 text-center text-red-500">
-        {error}
-      </div>
-    );
-  }
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-8">투두리스트</h1>
       <div className="max-w-md mx-auto mt-8">
+        <TodoForm />
         {/* <TodoForm loadTodos={loadTodos} /> */}
         <h2 className="text-2xl font-bold mb-4">할 일 목록</h2>
-        <TodoList todos={todos} />
+        <TodoList />
       </div>
     </div>
   );
