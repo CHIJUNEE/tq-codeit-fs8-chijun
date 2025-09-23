5da0913f8bfcabf8b7d867fbeeda22103dca6e6d
DESKTOP-14A5R6M\clwns <clwns00@naver.com>
2025-09-23 17:33:29 +0900

    enable 텟스트

--- src/app/_components/TodoList.js
+++ src/app/_components/TodoList.js
@@ -10,15 +10,28 @@ export default function TodoList() {
     data: todos,
     isPending: isLoading,
     error,
+    refetch,
   } = useQuery({
     queryKey: ["todos"],
     queryFn: fetchTodos,
+    enabled: false,
+    select: function (testobject) {
+      return testobject.filter((t) => t.completed);
+    },
     // staleTime: 5000,
   });
 
   if (isLoading) {
     return (
-      <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
+      <div className="container mx-auto px-4 py-8 text-center">
+        <button
+          className="bg-blue-400 text-white px-4 py-2 rounded-md"
+          onClick={refetch}
+        >
+          버튼
+        </button>
+        로딩 중...
+      </div>
     );
   } else if (error) {
     return (
